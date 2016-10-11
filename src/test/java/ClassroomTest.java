ed90cf2ddbd5e5e0e98f56469d1bab32b19a3b42
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-11 12:29:47 +0200

    Continued tests for class Classroom

--- src/test/java/ClassroomTest.java
+++ src/test/java/ClassroomTest.java
@@ -11,7 +11,9 @@ import otherclasses.Student;
 public class ClassroomTest {
 	ArrayList<Student> studList= new ArrayList<Student>();
 	Classroom cr = new Classroom("", "", studList);
-	
+	Student stud1 = new Student("Nisse", "Svensson", 35, 'M');
+	Student stud2 = new Student("Britta", "Persson", 32, 'F');
+		
 	@Test
 	public void testClassroomName() {
 		cr.setClassroomName("Test automation");
@@ -24,4 +26,19 @@ public class ClassroomTest {
 		assertEquals("Autumn 2016", cr.getClassroomTerm());
 	}
 
+	@Test
+	public void testAddANewStudent() {
+		studList.clear();
+		studList.add(stud1);
+		cr.addANewStudent(stud1);
+		assertEquals(studList,cr.getStudents());
+	}
+	
+	@Test
+	public void testStudents() {
+		studList.clear();
+		studList.add(stud1);
+		cr.setStudents(studList);
+		assertEquals(studList,cr.getStudents());
+	}
 }
