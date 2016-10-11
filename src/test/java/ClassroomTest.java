3e2d6138aee27127f08fa3ec6b0ac0f8dd066811
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-11 12:11:35 +0200

    Started with unit tests for class Classroom

--- src/test/java/ClassroomTest.java
+++ src/test/java/ClassroomTest.java
@@ -1,14 +1,27 @@
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import otherclasses.Classroom;
+import otherclasses.Student;
+
 
 public class ClassroomTest {
-
-	//Classroom cr = new Classroom();
+	ArrayList<Student> studList= new ArrayList<Student>();
+	Classroom cr = new Classroom("", "", studList);
 	
 	@Test
-	public void testClassroom() {
-		
+	public void testClassroomName() {
+		cr.setClassroomName("Test automation");
+		assertEquals("Test automation", cr.getClassroomName());
+	}
+
+	@Test
+	public void testClassroomTerm() {
+		cr.setClassroomTerm("Autumn 2016");
+		assertEquals("Autumn 2016", cr.getClassroomTerm());
 	}
 
 }
